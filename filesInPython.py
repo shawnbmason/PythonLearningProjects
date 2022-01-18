e35be088649489f23482f8d05052d8a731f4b76d
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-01-17 16:11:01 -0800

    hello python file

--- filesInPython.py
+++ filesInPython.py
@@ -109,3 +109,21 @@ import json
 
 with open("message_write.json", "w") as data_json:
   json.dump(data_payload, data_json)
+
+# import csv
+
+# compromised_users = []
+# with open('passwords.csv') as password_file:
+#   password_csv = csv.DictReader(password_file)
+#   for password_row in password_csv:
+#     compromised_users.append(password_row['Username'])
+  
+# with open("compromised_users.txt", "w") as compromised_user_file:
+#   for compromised_user in compromised_users:
+#     compromised_user_file.write(compromised_user)
+
+# import json
+
+# with open("boss_message.json", "w") as boss_message:
+#   boss_message_dict = {'recipient': 'The Boss', 'message': 'Mission Success'}
+#   json.dump(boss_message_dict, boss_message)
